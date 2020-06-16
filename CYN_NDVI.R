ae7576281b598ce792bcc37f018b8e435c45a185
eforbath <eforbath@colgate.edu>
2020-06-16 13:12:52 -0400

    NDVI

---
+++ CYN_NDVI.R
@@ -0,0 +1,12 @@
+########### Cherkskiy North Veg Removal Plots #########3
+
+getwd()
+
+install.packages("dplyr")
+install.packages("ggplot2")
+
+library(dplyr)
+library(ggplot2)
+
+## NDVI analyses ##
+
