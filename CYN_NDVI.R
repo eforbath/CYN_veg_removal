dc5d4dec186fa9de691de2a156df4fb5af71d2ec
eforbath <eforbath@colgate.edu>
2020-07-02 14:32:27 -0400

    added notes

--- CYN_NDVI.R
+++ CYN_NDVI.R
@@ -5,24 +5,24 @@ setwd("/Users/elenaforbath/Downloads/loranty_lab/data")
 
 install.packages("dplyr")
 install.packages("ggplot2")
-
-library(dplyr)
-library(ggplot2)
-
-########### NDVI analyses########### 
-
-## read in tiff file
 install.packages("tiff")
 install.packages("rtiff")
 install.packages("raster") 
 install.packages("sp")
 install.packages("rgdal")
+
+library(dplyr)
+library(ggplot2)
 library(tiff)
 library(rtiff)
 library(raster)
 library(sp)
 library(rgdal)
 
+
+########### NDVI analyses########### 
+
+## read in tiff file and create raster plots
 FL016 <- raster("CYN_TR1_FL016M/RU_CYN_TR1_FL016B_index_ndvi.tif")
 FL016b <- projectRaster(FL016, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs",
                         method = "bilinear", 
@@ -38,7 +38,7 @@ FL020b
 plot(FL020b)
 
 
-## histogram of pre/post NDVI values
+## histogram of pre/post NDVI values (overlapping values)
 
 c1 <- rgb(173,216,230,max = 255, alpha = 80, names = "lt.blue")
 c2 <- rgb(255,192,203, max = 255, alpha = 80, names = "lt.pink")
@@ -59,10 +59,6 @@ dev.off()
 GPS <- na.omit(read.csv("CYN_plot_centers.csv"))
 
 GPS2 <- subset(GPS, select= -c(plot, elevation))
-
-GPS2
-
-colnames(GPS2)
 
 ## re order columns so longitude is first
 GPS_order <- GPS2[,c("longitude", "latitude")]
@@ -92,31 +88,34 @@ ndvi_FL020 <- extract(FL020b,
 names(ndvi_FL020)[names(ndvi_FL020) == "NDVI.data"] <- "FL020_ndvi"
 ndvi_FL020b <- as.data.frame(ndvi_FL020)
 
-## plot of data points on ndvi maps
 
+## plot data points on ndvi maps
 plot(FL016b, 
      main = "Pre-Clipping NDVI", 
      xlab = "Longitude", 
      ylab = "Latitude")
 points(GPS_order$longitude, GPS_order$latitude, pch=20)
 
-plot(FL020b)
+plot(FL020b, 
+     main = "Post-Clipping NDVI", 
+     xlab = "Longitude", 
+     ylab = "Latitude")
 points(GPS_order$longitude, GPS_order$latitude, pch=20)
 
-## combine data frames by lat and long
+## combine pre- and post-clipping data frames by lat and long
 
-ndvi <- merge(ndvi_FL016b, ndvi_FL020b, by = c("longitude", "latitude"))
-ndvi
+ndvi <- merge(ndvi_FL016b, ndvi_FL020b, by = c("longitude", "latitude")) ## just pre- and post-
 
-ndvi_plots <- merge(ndvi, GPS, by = c("longitude", "latitude"))
-ndvi_plots
+ndvi_plots <- merge(ndvi, GPS, by = c("longitude", "latitude"))## add GPS points
 
-## rearrage column orders
+
+## rearrage column orders for ease of reading
 ndvi_plots2 <- ndvi_plots[,c("plot", "longitude", "latitude", "elevation", "FL016_ndvi", "FL020_ndvi")]
 
 
-## plotting NDVI differences
-df<-ndvi_plots2[, c("FL016_ndvi", "FL020_ndvi")]
+## plotting NDVI changes (all plots)
+df <- ndvi_plots2[, c("FL016_ndvi", "FL020_ndvi")] ##need this???
+
 barplot(cbind(FL016_ndvi, FL020_ndvi) ~ plot, 
         ndvi_plots2,
         beside = TRUE,
@@ -142,6 +141,7 @@ ndvi_CT <- subset(ndvi, treatment == "CT")
 ndvi_GR <- subset(ndvi, treatment == "GR")
 ndvi_SH <- subset(ndvi, treatment == "SH")
 ndvi_GS <- subset(ndvi, treatment == "GS")
+
 
 ## control ##
 
@@ -224,22 +224,19 @@ summary(aov)
 
 
 
-
 lm <- lm(FL020_ndvi ~ treatment, data = ndvi)
 summary(lm)
 aov <- aov(FL020_ndvi ~ treatment, data = ndvi)
 summary(aov)
 
-## subtract pre- and post- + linear regression 
+
+## subtract pre- and post- & linear regression 
 ndvi$ndvi_diff <- (ndvi$FL020_ndvi - ndvi$FL016_ndvi)
 lm <- lm(ndvi_diff ~ treatment, data = ndvi)
 summary(lm)
 
 
 ## subtracting pre- and post-clipping rasters 
-difference <- (FL020b - FL016b)
- ## Error in compareRaster(e1, e2, extent = FALSE, rowcol = FALSE, crs = TRUE,  : 
-  ## different resolution
 
 ## crop and change resolution of FL020
 FL020_crop <- crop(FL020b, FL016b)
